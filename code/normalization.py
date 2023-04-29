cf11419441cd16fef5f2b8f573e2065d094718f6
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2023-04-29 15:41:41 +0800

    feature_construction

--- code/normalization.py
+++ code/normalization.py
@@ -5,19 +5,22 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-# define data
-data = pd.read_csv('../data/data.csv', header=0, index_col=0)
 
-# normalize features using min-max normalization
-scaler = MinMaxScaler()
-data[['open', 'high', 'low', 'close', 'daily_trading_volume',
-      'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']] = scaler.fit_transform(data[['open', 'high', 'low', 'close', 'daily_trading_volume',
-                                                                                     'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']])
+def normalize_data():
+    # define data
+    data = pd.read_csv('../data/data.csv', header=0, index_col=0)
 
-# export Min and Max value of each feature to csv file
-column_index = [['open', 'high', 'low', 'close', 'daily_trading_volume',
-                 'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']]
-scaler_min_max_data = pd.DataFrame(
-    scaler.data_max_, index=column_index, columns=['Max'])
-scaler_min_max_data['Min'] = scaler.data_min_
-scaler_min_max_data.to_csv('../data/scaler.csv')
+    # normalize features using min-max normalization
+    scaler = MinMaxScaler()
+    data[['open', 'high', 'low', 'close', 'daily_trading_volume',
+          'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']] = scaler.fit_transform(data[['open', 'high', 'low', 'close', 'daily_trading_volume',
+                                                                                         'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']])
+
+    # export Min and Max value of each feature to csv file
+    column_index = [['open', 'high', 'low', 'close', 'daily_trading_volume',
+                    'RSI_14', 'BollW', 'precent_B', 'BIAS', 'ROC_1']]
+    scaler_min_max_data = pd.DataFrame(
+        scaler.data_max_, index=column_index, columns=['Max'])
+    scaler_min_max_data['Min'] = scaler.data_min_
+    scaler_min_max_data.to_csv('../data/scaler.csv')
+    return data
