ddc45dd704d69f5d69fbf2c5d3aae5a1914b4e77
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2023-05-01 23:52:36 +0800

    graph cnn total

--- code/model_evaluate.py
+++ code/model_evaluate.py
@@ -7,16 +7,16 @@ from sklearn.metrics import roc_curve, accuracy_score, precision_score, recall_s
 import os
 
 
-def predict_price(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100, model_num=0, good = 'NO'):
+def predict_price(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100, model_num=0, good='NO'):
     if model_type == 'CNN':
         if good == 'NO':
             modelname = '../model/cnn_model' + str(cnn) + '_seed' + str(seed) + '_epochs' + str(epochs) +\
-                    '_days' + str(n_days) + '_stride' + str(stride) + \
-                    '_diff' + str(diff) + '.h5'
+                '_days' + str(n_days) + '_stride' + str(stride) + \
+                '_diff' + str(diff) + '.h5'
         elif good == 'good':
             modelname = '../model/cnn_model' + str(cnn) + '_seed' + str(seed) + '_epochs' + str(epochs) +\
-            '_days' + str(n_days) + '_stride' + str(stride) + \
-            '_diff' + str(diff) + '_good.h5'
+                '_days' + str(n_days) + '_stride' + str(stride) + \
+                '_diff' + str(diff) + '_good.h5'
     elif model_type == 'LSTM':
         surname = f'lstm_model{model_num}_seed{seed}'
         modelname = '../model/' + surname + '.h5'
@@ -31,14 +31,15 @@ def predict_price(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=F
 
     # save result
     if model_type == 'LSTM':
-        np.savetxt(f'../prediction/{surname}.csv',y_pred)
+        np.savetxt(f'../prediction/{surname}.csv', y_pred)
     elif model_type == 'CNN':
         if good == 'good':
-            np.savetxt(f'../prediction/cnn_model{cnn}_seed{seed}_epochs{epochs}_days{n_days}_stride{stride}_diff{diff}_good.csv',y_pred)
+            np.savetxt(
+                f'../prediction/cnn_model{cnn}_seed{seed}_epochs{epochs}_days{n_days}_stride{stride}_diff{diff}_good.csv', y_pred)
 
-    return_pred_plot(y_test, y_pred, seed=seed, cnn=cnn, n_days=n_days, stride=stride, model_type=model_type, diff=diff, epochs=epochs)
-    price_pred_graph(y_pred, seed=seed, cnn=cnn, n_days=n_days,
-                     stride=stride, model_type=model_type, diff=diff, epochs=epochs)
+    # return_pred_plot(y_test, y_pred, seed=seed, cnn=cnn, n_days=n_days, stride=stride, model_type=model_type, diff=diff, epochs=epochs)
+    # price_pred_graph(y_pred, seed=seed, cnn=cnn, n_days=n_days,
+    #                  stride=stride, model_type=model_type, diff=diff, epochs=epochs)
     return y_test, y_pred
 
 # plot prediction only display 5 x-axis label
@@ -49,19 +50,19 @@ def return_pred_plot(y_test, y_pred, seed=4012, cnn=1, n_days=5, stride=1, model
     y_pred = pd.DataFrame(y_pred)
     y_test.index = range(len(y_test))
     y_pred.index = range(len(y_test))
-    plt.figure(figsize=(10, 8))
-    plt.plot(y_test.index, y_test, label='y_test')
-    plt.plot(y_test.index, y_pred, label='y_pred')
-    plt.xticks(y_test.index[::int(len(y_test) / 5)])
-    plt.legend()
-    filename = '../graph/better_model_cnn_ROC/return_cnn_model' + str(cnn) + '_return_pred_seed' + str(seed) + '_epochs' + str(epochs) +\
-        '_days' + str(n_days) + '_stride' + str(stride) + \
-        '_diff' + str(diff) + '.png'
-    plt.savefig(filename, dpi=1200, bbox_inches='tight')
+    # plt.figure(figsize=(10, 8))
+    # plt.plot(y_test.index, y_test, label='y_test')
+    # plt.plot(y_test.index, y_pred, label='y_pred')
+    # plt.xticks(y_test.index[::int(len(y_test) / 5)])
+    # plt.legend()
+    # filename = '../graph/better_model_cnn_ROC/return_cnn_model' + str(cnn) + '_return_pred_seed' + str(seed) + '_epochs' + str(epochs) +\
+    #     '_days' + str(n_days) + '_stride' + str(stride) + \
+    #     '_diff' + str(diff) + '.png'
+    # plt.savefig(filename, dpi=1200, bbox_inches='tight')
     # plt.show()
 
 
-def price_pred_graph(return_pred, seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100):
+def price_pred_graph(return_pred, seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100, good='NO'):
     return_pred = pd.DataFrame(return_pred)
     close = pd.read_csv('../data/data.csv', index_col=0)['close']
     y_true = close.iloc[-276:-6]
@@ -74,20 +75,24 @@ def price_pred_graph(return_pred, seed=4012, cnn=1, n_days=5, stride=1, model_ty
         y_pred.append(y_pred[i-1]*(1+float(return_pred.iloc[i-1].values)))
         i += 1
 
-    plt.figure(figsize=(10, 8))
-    plt.plot(y_true, label='close')
-    plt.plot(y_pred, label='pred')
-    plt.xticks(y_true.index[::int(len(y_true) / 5)])
-    plt.legend()
-    filename = '../graph/better_model_cnn_ROC/price_cnn_model' + str(cnn) + '_price_pred_seed' + str(seed) + '_epochs' + str(epochs) +\
-        '_days' + str(n_days) + '_stride' + str(stride) + \
-        '_diff' + str(diff) + '.png'
-    plt.savefig(filename, dpi=1200, bbox_inches='tight')
+    # plt.figure(figsize=(10, 8))
+    # plt.plot(y_true, label='close')
+    # label = 'cnn_model' + str(cnn) + '_seed' + str(seed) + '_epochs' + str(epochs) +\
+    #     '_days' + str(n_days) + '_stride' + str(stride) + \
+    #     '_diff' + str(diff)
+    # plt.plot(y_pred, label=label)
+    # plt.xticks(y_true.index[::int(len(y_true) / 5)])
+    # plt.legend()
+    # filename = '../graph/better_model_cnn_ROC/price_cnn_model' + str(cnn) + '_price_pred_seed' + str(seed) + '_epochs' + str(epochs) +\
+    #     '_days' + str(n_days) + '_stride' + str(stride) + \
+    #     '_diff' + str(diff) + '.png'
+    # plt.savefig(filename, dpi=1200, bbox_inches='tight')
     # plt.show()
 
 
-def CNN_classification(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100, model_num=0, good = 'NO'):
-    y_test, y_pred = predict_price(seed=seed, cnn=cnn, n_days=n_days, stride=stride, model_type=model_type, diff=diff, epochs=epochs, good=good)
+def CNN_classification(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', diff=False, epochs=100, model_num=0, good='NO'):
+    y_test, y_pred = predict_price(seed=seed, cnn=cnn, n_days=n_days, stride=stride,
+                                   model_type=model_type, diff=diff, epochs=epochs, good=good)
     y_pred_prob = 1 / (1 + np.exp(-y_pred))
 
     y_pred_class = np.where(y_pred_prob > 0.5, 1, 0)
@@ -100,16 +105,16 @@ def CNN_classification(seed=4012, cnn=1, n_days=5, stride=1, model_type='CNN', d
     print('Confusion Matirx: ', confusion_matrix(y_test_class, y_pred_class))
 
     # plot roc curve
-    fpr, tpr, thresholds = roc_curve(y_test_class, y_pred_prob)
-    plt.figure(figsize=(10, 8))
-    plt.plot(fpr, tpr)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    filename = '../graph/better_model_cnn_ROC/ROC_cnn_model' + str(cnn) + '_ROC_seed' + str(seed) + '_epochs' + str(epochs) +\
-        '_days' + str(n_days) + '_stride' + str(stride) + \
-        '_diff' + str(diff) + '.png'
-    plt.savefig(filename, dpi=1200, bbox_inches='tight')
+    # fpr, tpr, thresholds = roc_curve(y_test_class, y_pred_prob)
+    # plt.figure(figsize=(10, 8))
+    # plt.plot(fpr, tpr)
+    # plt.plot([0, 1], [0, 1], 'k--')
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # filename = '../graph/better_model_cnn_ROC/ROC_cnn_model' + str(cnn) + '_ROC_seed' + str(seed) + '_epochs' + str(epochs) +\
+    #     '_days' + str(n_days) + '_stride' + str(stride) + \
+    #     '_diff' + str(diff) + '.png'
+    # plt.savefig(filename, dpi=1200, bbox_inches='tight')
     # plt.show()
     return y_test_class, y_pred_prob
 
@@ -134,6 +139,8 @@ def plot_ROC_and_pred():
     # plot all ROC curve in one plot
     plt.figure(figsize=(10, 8))
     for i in range(len(model_list)):
+        # print model info
+        print('model: ', model_list[i])
         y_test_class, y_pred_prob = CNN_classification(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
             stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
         fpr, tpr, thresholds = roc_curve(y_test_class, y_pred_prob)
@@ -144,25 +151,39 @@ def plot_ROC_and_pred():
     plt.legend()
     plt.show()
 
-    # plot all predicted return in one plot
-    plt.figure(figsize=(10, 8))
-    for i in range(len(model_list)):
-        y_test, y_pred = predict_price(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
-            stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
-        if i == 0:
-            plt.plot(y_test, label='true')
-        plt.plot(y_pred, label=model_list[i])
+    # # plot all predicted return in one plot
+    # plt.figure(figsize=(10, 8))
+    # for i in range(len(model_list)):
+    #     y_test, y_pred = predict_price(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
+    #         stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
+    #     # make y_test to pd.Series and give index as close price index
+    #     close = pd.read_csv('../data/data.csv', index_col=0)['close']
+    #     y_true = close.iloc[-276:-6]
+    #     y_test = pd.Series(y_test, index=y_true.index)
+    #     if i == 0:
+    #         plt.plot(y_test, label='true')
+    #     plt.plot(y_pred, label=model_list[i])
     # plt.xticks(y_test.index[::int(len(y_test) / 5)])
-    plt.legend()
-    plt.show()
+    # plt.legend()
+    # plt.show()
 
+    # plot all predicted price in one plot
+    # plt.figure(figsize=(10, 8))
+    # for i in range(len(model_list)):
+    #     y_test, y_pred = predict_price(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
+    #         stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
+    #     if i == 0:
+    #         close = pd.read_csv('../data/data.csv', index_col=0)['close']
+    #         y_true = close.iloc[-276:-6]
+    #         plt.plot(y_true, label='true')
 
-
-
-
-
-
-
+    #     price_pred_graph(y_pred, seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
+    #         stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
+    # plt.xticks(y_true.index[::int(len(y_test) / 10)])
+    # # rotate xticks
+    # plt.xticks(rotation=45)
+    # plt.legend()
+    # plt.show()
 
 
 if __name__ == "__main__":
@@ -180,27 +201,25 @@ if __name__ == "__main__":
     # y_test, y_pred = predict_price(seed=998, epochs=100, cnn=2, n_days=10, stride=1, model_type='CNN', diff=False, good='good')
     # y_test, y_pred = predict_price(seed=998, epochs=100, cnn=2, n_days=5, stride=1, model_type='CNN', diff=False, good='good')
     # y_test, y_pred = predict_price(seed=61, epochs=100, cnn=4, n_days=5, stride=1, model_type='CNN', diff=True, good='good')
-    # plot_ROC_and_pred()
-    model_list = os.listdir('../model/good_model_cnn/better_model_cnn_ROC')
-    model_list = [i for i in model_list if i[-3:] == '.h5']
-    # extract seed, epochs, cnn, n_days, stride, model_type, diff from model name
-    model_info = []
-    for i in model_list:
-        model_info.append(i.split('_'))
-    model_info = pd.DataFrame(model_info)
-    cnn = model_info[1].str.extract('(\d+)')
-    n_days = model_info[4].str.extract('(\d+)')
-    stride = model_info[5].str.extract('(\d+)')
-    # diff will not include first 4 characters 'diff'
-    diff = model_info[6].str[4:]
-    epochs = model_info[3].str.extract('(\d+)')
-    seed = model_info[2].str.extract('(\d+)')
-    model_type = 'CNN'
+    plot_ROC_and_pred()
+    # model_list = os.listdir('../model/good_model_cnn/better_model_cnn_ROC')
+    # model_list = [i for i in model_list if i[-3:] == '.h5']
+    # # extract seed, epochs, cnn, n_days, stride, model_type, diff from model name
+    # model_info = []
+    # for i in model_list:
+    #     model_info.append(i.split('_'))
+    # model_info = pd.DataFrame(model_info)
+    # cnn = model_info[1].str.extract('(\d+)')
+    # n_days = model_info[4].str.extract('(\d+)')
+    # stride = model_info[5].str.extract('(\d+)')
+    # # diff will not include first 4 characters 'diff'
+    # diff = model_info[6].str[4:]
+    # epochs = model_info[3].str.extract('(\d+)')
+    # seed = model_info[2].str.extract('(\d+)')
+    # model_type = 'CNN'
 
-    for i in range(len(model_list)):
-        # print model info
-        print('model name: ', model_list[i])
-        CNN_classification(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
-            stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
-    
-
+    # for i in range(len(model_list)):
+    #     # print model info
+    #     print('model name: ', model_list[i])
+    #     CNN_classification(seed=int(seed.iloc[i]), cnn=int(cnn.iloc[i]), n_days=int(n_days.iloc[i]), stride=int(
+    #         stride.iloc[i]), model_type=model_type, diff=diff.iloc[i], epochs=int(epochs.iloc[i]), good='good')
